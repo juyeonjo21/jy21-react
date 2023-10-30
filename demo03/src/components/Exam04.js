c147a46def084e2938107622491c927102ea5be1
juyeonjo21 <merry2j@naver.com>
2023-10-30 22:44:29 +0900

    textarea 글자수 제한 문제 - 과제

--- demo03/src/components/Exam04.js
+++ demo03/src/components/Exam04.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 
 const Exam04 = ()=>{
+    const [inputText, setInputText] = useState('');
 
+    const maxLength = 1000;
+
+const onTextareaHandler = (e) => {
+    const text = e.target.value;
+
+if (text.length <= maxLength) {
+    setInputText(text);
+  }
+};
     return(
         <>
         <h1>10/30 과제풀기</h1>
         <hr/>
-        <h2>(Q) 주말에 뭐하세요?</h2>
-        <textarea type="range"></textarea>
+        <h3>(Q) 주말에 뭐하세요?</h3>
+        <textarea className="form-input w-50" rows={10} value={inputText} onChange={onTextareaHandler}/>
+        <br/>
+        <p>
+            <span>{inputText.length}</span>
+            <span>/1000 byte</span>
+        </p>
         </>
     );
 };
