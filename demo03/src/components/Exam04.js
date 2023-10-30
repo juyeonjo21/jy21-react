cfa5ad232790f3e9dcc60e33851442681711b982
juyeonjo21 <merry2j@naver.com>
2023-10-30 18:18:28 +0900

    10/30 과제 작성 중

---
+++ demo03/src/components/Exam04.js
@@ -0,0 +1,14 @@
+
+const Exam04 = ()=>{
+
+    return(
+        <>
+        <h1>10/30 과제풀기</h1>
+        <hr/>
+        <h2>(Q) 주말에 뭐하세요?</h2>
+        <textarea type="range"></textarea>
+        </>
+    );
+};
+
+export default Exam04;
