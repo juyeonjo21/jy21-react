950e91f12dcafc463153a72360810e8a2a14071e
juyeonjo21 <merry2j@naver.com>
2023-10-31 10:17:43 +0900

    자기소개서 문제 풀이

--- demo03/src/components/Exam04.js
+++ demo03/src/components/Exam04.js
@@ -1,29 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const Exam04 = ()=>{
+const Exam04 = () => {
     const [inputText, setInputText] = useState('');
+    const [length, setLength] = useState(0);
 
-    const maxLength = 1000;
+    // state끼리 의존성이 생기는 경우가 있다
+    //- content가변하면 length가 변해야 한다
+    //- 수동이 아니라 자동으로 변하도록 설정할 수 있다
+    //=> iseEffect 훅 사용
 
-const onTextareaHandler = (e) => {
-    const text = e.target.value;
+    //- useEffect(gkatn,[감지항목]);
+    useEffect(() => {
+        setLength(inputText.length);
+    }, [inputText]);
 
-if (text.length <= maxLength) {
-    setInputText(text);
-  }
-};
-    return(
-        <>
-        <h1>10/30 과제풀기</h1>
-        <hr/>
-        <h3>(Q) 주말에 뭐하세요?</h3>
-        <textarea className="form-input w-50" rows={10} value={inputText} onChange={onTextareaHandler}/>
-        <br/>
-        <p>
-            <span>{inputText.length}</span>
-            <span>/1000 byte</span>
-        </p>
-        </>
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-10 offset-md-1">
+
+                    <div className="row">
+                        <div className="col">
+                            <h1>네 번째 예제</h1>
+                        </div>
+                    </div>
+
+                    <div className="row mt-2">
+                        <div className="col">
+                            <h2>(Q) 주말에 뭐하세요?</h2>
+                        </div>
+                    </div>
+
+                    <div className="row mt-2">
+                        <div className="col">
+                            <textarea name="inputText" className="form-control" rows={6} value={inputText} onChange={e => setInputText(e.target.value)} />
+                        </div>
+                    </div>
+
+                    <div className="row mt-2">
+                        <div className="col text-end">
+                            {inputText.length}/1000
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 };
 
