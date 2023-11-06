f7fb6f46278a66d1c762f0637f59cfc9744e8bdb
juyeonjo21 <merry2j@naver.com>
2023-11-06 18:18:24 +0900

    몬스터 등록 구현중

--- demo05/src/components/Pocketmon.js
+++ demo05/src/components/Pocketmon.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { LiaEdit } from "react-icons/lia";
+import { AiOutlinePlus } from "react-icons/ai";
+import { Modal } from "bootstrap";
 
 import axios from "axios";
 const Pocketmon = (props) => {
@@ -26,7 +28,7 @@ const Pocketmon = (props) => {
     //- 이제는 state에서 삭제하는 것이 아니라 서버에 통신을 보낸 뒤 목록을 갱신하면 된다
     const deletePocketmon = (pocketmon) => {
         const choice = window.confirm("정말 삭제하시겠습니까?");
-        if(choice === false) return;
+        if (choice === false) return;
 
 
         //axios({옵션}).then(성공 시 실행할 함수).catch(실패 시 실행할 함수);
@@ -40,6 +42,31 @@ const Pocketmon = (props) => {
             .catch(err => { });
     };
 
+    //modal 관련된 처리
+    const bsModal = useRef();
+    const openModal = () => {
+        const modal = new Modal(bsModal.current);
+        modal.show();
+    };
+
+    const closeModal = () => {
+        const modal = Modal.getInstance(bsModal.current);
+        modal.hide();
+    };
+
+    //등록과 관련된 state
+    const[pocketmon, setPocketmon] = useState({
+        name:"",
+        type:"",
+    });
+    const changePocketmon = (e)=>{
+        setPocketmon({
+            ...pocketmon,
+            [e.target.name] : e.target.value
+        });
+    };
+
+
     return (
         <>
             <div className="row">
@@ -49,6 +76,16 @@ const Pocketmon = (props) => {
                 </div>
             </div>
 
+            {/* 추가 버튼 */}
+            <div className="row">
+                <div className="col text-end">
+                    <button className="btn btn-success" onClick={openModal}>
+                        <AiOutlinePlus /> 추가
+                    </button>
+                </div>
+            </div>
+
+            {/* 출력 위치 */}
             <div className="row mt-4">
                 <div className="col">
                     <table className="table table-hover">
@@ -79,6 +116,44 @@ const Pocketmon = (props) => {
                     </table>
                 </div>
             </div>
+
+            {/* Modal */}
+            <div className="modal fade" ref={bsModal}
+                tabindex="-1" role="dialog" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">제목</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label">이름</label>
+                                    <input type="text" name="name" className="form-control"
+                                     value={pocketmon.name} onChange={changePocketmon} />
+                                </div>
+                            </div>
+
+                            <div className="row mt-4">
+                                <div className="col">
+                                    <label className="form-label">속성</label>
+                                    <input type="text" name="type" className="form-control"
+                                     value={pocketmon.type} onChange={changePocketmon} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-dark" onClick={closeModal}>닫기</button>
+                            <button className="btn btn-success">저장</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 
