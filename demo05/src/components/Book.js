46af805b382a1e1e614f83cd795ff195adf84653
juyeonjo21 <merry2j@naver.com>
2023-11-06 17:48:12 +0900

    삭제 구현

--- demo05/src/components/Book.js
+++ demo05/src/components/Book.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaXmark } from "react-icons/fa6";
+import { AiFillDelete } from "react-icons/ai";
 import { LiaEdit } from "react-icons/lia";
 import axios from "axios";
 
@@ -8,8 +8,7 @@ import "./Book.css";
 const Book = (props) => {
     const [bookList, setBookList] = useState([]);
 
-    useEffect(() => {
-        //서버에 있는 도서 정보를 불러와서 state에 반영하는 코드
+    const loadDeleteBook = () => {
         axios({
             url: "http://localhost:8080/book/",
             method: "get"
@@ -18,64 +17,84 @@ const Book = (props) => {
                 // console.log(response);
                 setBookList(response.data);
             })
-            .catch(err => {
-                window.alert("통신 오류 발생");
-            });
+            .catch(err => { });
+    }
+    useEffect(() => {
+        loadDeleteBook();
     }, []);
-    return (
-        <>
-            <div className="row">
-                <div className="col">
-                    <h1>도서 관리 화면</h1>
+
+
+    const deleteBook = (book) => {
+        const choice = window.confirm("정말 삭제하시겠습니까?");
+        if (choice === false) return;
+
+
+        //서버에 있는 도서 정보를 불러와서 state에 반영하는 코드
+        axios({
+            // url: "http://localhost:8080/book/"+book.bookId,
+            url: `http://localhost:8080/book/${book.bookId}`, //jsp에서 못 씀
+            method: "delete"
+        })
+            .then(response => {
+                loadDeleteBook();
+            })
+            .catch(err => { });
+
+     };
+        return (
+            <>
+                <div className="row">
+                    <div className="col">
+                        <h1>도서 관리 화면</h1>
+                    </div>
                 </div>
-            </div>
 
-            <div className="row mt-4">
-                <div className="col">
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th className="pc-only">No.</th>
-                                <th>도서명</th>
-                                <th className="pc-only">저자</th>
-                                <th>출간일</th>
-                                <th className="pc-only">판매가</th>
-                                <th>출판사</th>
-                                <th>페이지수</th>
-                                <th>장르</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {bookList.map((book, index) => (
-                                <tr key={book.bookId}>
-                                    <td className="pc-only">{book.bookId}</td>
-                                    <td className="pc-only">{book.bookTitle}</td>
-                                    <td className="pc-only">{book.bookAuthor}</td>
-                                    <td>{book.bookPublicationDate}</td>
-                                    <td className="pc-only">{book.bookPrice}</td>
-                                    <td>{book.bookPublisher}</td>
-                                    <td>{book.bookPageCount}</td>
-                                    <td>{book.bookGenre}</td>
-                                    <td>
-                                        {/* 아이콘 자리 */}
-                                        {/* 수정버튼 */}
-                                        <LiaEdit className="text-warning ms-1" />
-                                        {/* 삭제버튼 */}
-                                        <FaXmark className="text-danger ms-1" />
-                                    </td>
-
+                <div className="row mt-4">
+                    <div className="col">
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th className="pc-only">No.</th>
+                                    <th>도서명</th>
+                                    <th className="pc-only">저자</th>
+                                    <th>출간일</th>
+                                    <th className="pc-only">판매가</th>
+                                    <th>출판사</th>
+                                    <th>페이지수</th>
+                                    <th>장르</th>
+                                    <th></th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {bookList.map((book, index) => (
+                                    <tr key={book.bookId}>
+                                        <td className="pc-only">{book.bookId}</td>
+                                        <td className="pc-only">{book.bookTitle}</td>
+                                        <td className="pc-only">{book.bookAuthor}</td>
+                                        <td>{book.bookPublicationDate}</td>
+                                        <td className="pc-only">{book.bookPrice}</td>
+                                        <td>{book.bookPublisher}</td>
+                                        <td>{book.bookPageCount}</td>
+                                        <td>{book.bookGenre}</td>
+                                        <td>
+                                            {/* 아이콘 자리 */}
+                                            {/* 수정버튼 */}
+                                            <LiaEdit className="text-warning ms-1" />
+                                            {/* 삭제버튼 */}
+                                            <AiFillDelete className="text-danger ms-1" onClick={deleteBook} />
+                                        </td>
+
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
 
-        </>
+            </>
 
-    );
+        );
 
-};
+    };
 
-export default Book;
+    export default Book;
--- demo05/src/components/Pocketmon.js
+++ demo05/src/components/Pocketmon.js
@@ -1,53 +1,84 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-const Pocketmon = (props)=>{
-    const[pocketmonList, setPocketmonList] = useState([]);
-    useEffect(()=>{
-        //서버에서 pocketmon list를 불러와서 state에 설정하는 코드
-        axios({
-            url:"http://localhost:8080/pocketmon/",
-            method:"get"
-        })
-        .then(response=>{
-            // console.log(response);
-            setPocketmonList(response.data);
-        })
-        .catch(err=>{});
-    },[]);
-    
-    return(
-        <>
-        <div className="row">
-            <div className="col">
-        <h1>포켓몬 관리 화면</h1>
-        <p>React CRUD 연습 예제</p>
-            </div>
-        </div>
+import { AiFillDelete } from "react-icons/ai";
+import { LiaEdit } from "react-icons/lia";
 
-        <div className="row mt-4">
-            <div className="col">
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>번호</th>
-                            <th>이름</th>
-                            <th>속성</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {pocketmonList.map(pocketmon=>(
-                            <tr key={pocketmon.no}>
-                                <td>{pocketmon.no}</td>
-                                <td>{pocketmon.name}</td>
-                                <td>{pocketmon.type}</td>
-                                <td></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+import axios from "axios";
+const Pocketmon = (props) => {
+    const [pocketmonList, setPocketmonList] = useState([]);
+    //서버에서 pocketmon list를 불러와서 state에 설정하는 코드
+    const loadPocketmon = () => {
+        axios({
+            url: "http://localhost:8080/pocketmon/",
+            method: "get"
+        })
+            .then(response => {
+                // console.log(response);
+                setPocketmonList(response.data);
+            })
+            .catch(err => { });
+    };
+
+    useEffect(() => {
+        loadPocketmon();
+    }, []);
+
+    //포켓몬스터 삭제
+    //- 이제는 state에서 삭제하는 것이 아니라 서버에 통신을 보낸 뒤 목록을 갱신하면 된다
+    const deletePocketmon = (pocketmon) => {
+        const choice = window.confirm("정말 삭제하시겠습니까?");
+        if(choice === false) return;
+
+
+        //axios({옵션}).then(성공 시 실행할 함수).catch(실패 시 실행할 함수);
+        axios({
+            url: `http://localhost:8080/pocketmon/${pocketmon.no}`,
+            method: "delete"
+        })
+            .then(response => {
+                loadPocketmon(); //목록 갱신
+            })
+            .catch(err => { });
+    };
+
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    <h1>포켓몬 관리 화면</h1>
+                    <p>React CRUD 연습 예제</p>
+                </div>
             </div>
-        </div>
+
+            <div className="row mt-4">
+                <div className="col">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>번호</th>
+                                <th>이름</th>
+                                <th>속성</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {pocketmonList.map(pocketmon => (
+                                <tr key={pocketmon.no}>
+                                    <td>{pocketmon.no}</td>
+                                    <td>{pocketmon.name}</td>
+                                    <td>{pocketmon.type}</td>
+                                    <td>
+                                        {/* 아이콘 자리 */}
+                                        {/* 수정버튼 */}
+                                        <LiaEdit className="text-warning ms-1" />
+                                        {/* 삭제버튼 */}
+                                        <AiFillDelete className="text-danger ms-1" onClick={deletePocketmon} />
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     );
 
